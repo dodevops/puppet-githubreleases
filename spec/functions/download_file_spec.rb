03d0a827466f3a8307a9957c5f7620985ae0c3ae
Dennis Ploeger <dennis.ploeger@kps.com>
2020-01-28 15:53:48 +0100

    feat: Updated testsuite for internal function

---
+++ spec/functions/download_file_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'download_file' do
+  it 'does not raise an error' do
+    is_expected.not_to run.with_params(
+      'https://github.com/puppetlabs/puppet/archive/master.zip',
+      '/tmp/test.zip'
+    ).and_raise_error(/.*/)
+  end
+
+  it 'raises an error on an invalid URL' do
+    is_expected.to run.with_params(
+      'https://notmtiemtie.öäöä',
+      '/tmp/nothing'
+    ).and_raise_error(/URI must be ascii only/)
+  end
+
+  it 'raises an error on a not existing URL' do
+    is_expected.to run.with_params(
+      'https://github.com/nothingexistsherehopefully',
+      '/tmp/nothing'
+    ).and_raise_error(/404 Not Found/)
+  end
+end
