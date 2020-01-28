40293cd8491bd169245de0635d08aa82949658f9
Dennis Ploeger <dennis.ploeger@kps.com>
2020-01-28 15:55:21 +0100

    fix: Rubocop

--- spec/functions/download_file_spec.rb
+++ spec/functions/download_file_spec.rb
@@ -6,21 +6,21 @@ describe 'download_file' do
   it 'does not raise an error' do
     is_expected.not_to run.with_params(
       'https://github.com/puppetlabs/puppet/archive/master.zip',
-      '/tmp/test.zip'
-    ).and_raise_error(/.*/)
+      '/tmp/test.zip',
+    ).and_raise_error(%r{.*})
   end
 
   it 'raises an error on an invalid URL' do
     is_expected.to run.with_params(
       'https://notmtiemtie.öäöä',
-      '/tmp/nothing'
-    ).and_raise_error(/URI must be ascii only/)
+      '/tmp/nothing',
+    ).and_raise_error(%r{URI must be ascii only})
   end
 
   it 'raises an error on a not existing URL' do
     is_expected.to run.with_params(
       'https://github.com/nothingexistsherehopefully',
-      '/tmp/nothing'
-    ).and_raise_error(/404 Not Found/)
+      '/tmp/nothing',
+    ).and_raise_error(%r{404 Not Found})
   end
 end
