777704f1a0a4b683601b4460b568010185d4dc30
Marc Schöchlin <schoechlin@osb-alliance.com>
2023-11-09 13:27:53 +0100

    fix: follow redirects, close #3

--- lib/puppet/provider/githubreleases_download/githubreleases_download.rb
+++ lib/puppet/provider/githubreleases_download/githubreleases_download.rb
@@ -6,8 +6,20 @@ Puppet::Type.type('githubreleases_download').provide(:githubreleases_download) d
 
   def create
     download_url = get_download_url(@resource[:author], @resource[:repository], @resource)
-    download = open(download_url)
-    IO.copy_stream(download, @resource[:target])
+    uri = URI.parse(download_url)
+
+    # Follow redirects
+    response = Net::HTTP.get_response(uri)
+
+    while [301, 302].include?(response.code.to_i))
+      uri = URI.parse(response['location'])
+      response = Net::HTTP.get_response(uri)
+    end
+
+    # Download the file
+    open(@resource[:target], 'wb') do |file|
+      file << response.body
+    end
   end
 
   def destroy
